5a8a5634b4671985611e4b2dc13c537099afb9d0
Muhammad Hazimi Bin Yusri <126186721+Muhammad-Hazimi-Yusri@users.noreply.github.com>
2024-03-12 00:24:47 +0000

    SEARCH BAR WORKS, and next plan in logbook
    basic search bar works, check all 3 logbook for update and next plan, basically need to process and analyse each week output for easier time in godot

--- Software_Game/2D file browser and viewer/scripts/file_explorer.gd
+++ Software_Game/2D file browser and viewer/scripts/file_explorer.gd
@@ -43,7 +43,7 @@ func open_file(file_name:String):
 	Npath.text = path + "/" + file_name
 	file = true
 
-func set_layout():
+func set_layout(search_text: String =""):
 	file = false
 	Npath.text = path
 	for i in cont.get_children(): i.queue_free()
@@ -51,60 +51,62 @@ func set_layout():
 	var dir=DirAccess.open(path)
 	dir.list_dir_begin()
 	var file_name = dir.get_next()
+	
+	while file_name != "": 
+		if search_text == "" or file_name.contains(search_text): #caps sensitive
+			if dir.current_is_dir():
+				var nBut = Button.new()
+				nBut.text = file_name
+				cont.add_child(nBut)
+				nBut.pressed.connect(open_folder.bind(file_name))
+				nBut.add_theme_stylebox_override("normal", load("res://folder_box.tres"))
+			else:
+				var file_container = VBoxContainer.new()
+				cont.add_child(file_container)
 
-	while file_name != "":
-		if dir.current_is_dir():
-			var nBut = Button.new()
-			nBut.text = file_name
-			cont.add_child(nBut)
-			nBut.pressed.connect(open_folder.bind(file_name))
-			nBut.add_theme_stylebox_override("normal", load("res://folder_box.tres"))
-		else:
-			var file_container = VBoxContainer.new()
-			cont.add_child(file_container)
+				var texture_rect = TextureRect.new()
+				file_container.add_child(texture_rect)
 
-			var texture_rect = TextureRect.new()
-			file_container.add_child(texture_rect)
+				if file_name.get_extension() in limited:
 
-			if file_name.get_extension() in limited:
+					if file_name.get_extension() == "jpg" or file_name.get_extension() == "png":
+						
+						
+						# Image file handling
+						var image_texture = ImageTexture.create_from_image(Image.load_from_file(path + "/" + file_name))
+						image_texture.set_size_override(Vector2i(128, 128))
+						texture_rect.texture = image_texture
+					
+					elif file_name.get_extension() == "ogv":
+						
+						# Video file handling
+						#print_debug("Video is here")
+						var hidden_screen = TextureRect.new()
+						cont.add_child(hidden_screen)
+						
+						var video_player = VideoStreamPlayer.new()
+						hidden_screen.add_child(video_player)
+						hidden_screen.hide()
+						video_player.stream = load(path + "/" + file_name)
+						video_player.play()
+						
+						await get_tree().create_timer(0.1).timeout  # Wait for the first frame to be rendered
+						
+						var thumbnail = video_player.get_video_texture().get_image()
+						var thumbnail_texture = ImageTexture.create_from_image(thumbnail)
+						thumbnail_texture.set_size_override(Vector2i(128, 128))
+						texture_rect.texture = thumbnail_texture
+						
+						video_player.queue_free()
+						
+				var nBut = Button.new()
+				nBut.text = file_name
+				file_container.add_child(nBut)
+				nBut.pressed.connect(open_file.bind(file_name))
 
-				if file_name.get_extension() == "jpg" or file_name.get_extension() == "png":
-					
-					# Image file handling
-					var image_texture = ImageTexture.create_from_image(Image.load_from_file(path + "/" + file_name))
-					image_texture.set_size_override(Vector2i(128, 128))
-					texture_rect.texture = image_texture
-				
-				elif file_name.get_extension() == "ogv":
-					
-					# Video file handling
-					#print_debug("Video is here")
-					var hidden_screen = TextureRect.new()
-					cont.add_child(hidden_screen)
-					
-					var video_player = VideoStreamPlayer.new()
-					hidden_screen.add_child(video_player)
-					hidden_screen.hide()
-					video_player.stream = load(path + "/" + file_name)
-					video_player.play()
-					
-					await get_tree().create_timer(0.1).timeout  # Wait for the first frame to be rendered
-					
-					var thumbnail = video_player.get_video_texture().get_image()
-					var thumbnail_texture = ImageTexture.create_from_image(thumbnail)
-					thumbnail_texture.set_size_override(Vector2i(128, 128))
-					texture_rect.texture = thumbnail_texture
-					
-					video_player.queue_free()
-					
-			var nBut = Button.new()
-			nBut.text = file_name
-			file_container.add_child(nBut)
-			nBut.pressed.connect(open_file.bind(file_name))
-
-			if limited.size() > 0:
-				if !file_name.get_extension() in limited:
-					file_container.queue_free()
+				if limited.size() > 0:
+					if !file_name.get_extension() in limited:
+						file_container.queue_free()
 
 		file_name = dir.get_next()
 
@@ -152,3 +154,6 @@ func _on_cancelf_pressed():
 
 func _on_new_folder_pressed():
 	$add_folder.show()
+
+func _on_search_bar_text_submitted(new_text): # used submitted instead of changed due to perfomances issue from reloading thumbnails (typing too slow)
+	set_layout(new_text)
