6e5a5aacfa7375bff71a6a9025866c06d74d03b0
Muhammad Hazimi Bin Yusri <126186721+Muhammad-Hazimi-Yusri@users.noreply.github.com>
2024-03-12 04:02:47 +0000

    IMAGE FILTER WORKS, v7.exe, video filter OTW

--- Software_Game/2D file browser and viewer/scripts/file_explorer.gd
+++ Software_Game/2D file browser and viewer/scripts/file_explorer.gd
@@ -4,6 +4,9 @@ extends ColorRect
 @onready var pinned = $pinned/pinned_container
 @onready var Npath = $path
 
+# Add this variable to store the selected filter index
+var selected_filter_index = 0
+
 var path:String = ""
 var file:bool = false
 
@@ -12,7 +15,7 @@ var limited = []
 signal done(path:String)
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready():    
 	path = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
 	set_layout()
 	for i in Globals.pinned:
@@ -42,18 +45,22 @@ func open_file(file_name:String):
 	#path = path + "/" + file_name # commented and modifief line below as fix for crash from recalling videoplayer.load at wrong path again for some reason at line 88
 	Npath.text = path + "/" + file_name
 	file = true
-
-func set_layout(search_text: String =""):
+	
+func set_layout(search_text: String ="", repopulate_dropdown: bool = false):
 	file = false
 	Npath.text = path
 	for i in cont.get_children(): i.queue_free()
 	
-	var dir=DirAccess.open(path)
+	var dir = DirAccess.open(path)
 	dir.list_dir_begin()
 	var file_name = dir.get_next()
 	
-	while file_name != "": 
-		if search_text == "" or file_name.contains(search_text): #caps sensitive
+	# Get the selected filter option
+	var selected_filter = $metadata_filters.get_item_text(selected_filter_index)
+	print_debug("Selected filter is:"+selected_filter)
+	
+	while file_name != "":
+		if search_text == "" or file_name.contains(search_text):
 			if dir.current_is_dir():
 				var nBut = Button.new()
 				nBut.text = file_name
@@ -61,65 +68,95 @@ func set_layout(search_text: String =""):
 				nBut.pressed.connect(open_folder.bind(file_name))
 				nBut.add_theme_stylebox_override("normal", load("res://folder_box.tres"))
 			else:
-				var file_container = VBoxContainer.new()
-				cont.add_child(file_container)
-
-				var texture_rect = TextureRect.new()
-				file_container.add_child(texture_rect)
-
-				if file_name.get_extension() in limited:
-
-					if file_name.get_extension() == "jpg" or file_name.get_extension() == "png":
-						
-						
-						# Image file handling
-						var image_texture = ImageTexture.create_from_image(Image.load_from_file(path + "/" + file_name))
-						image_texture.set_size_override(Vector2i(128, 128))
-						texture_rect.texture = image_texture
-						
-						# metadata filters dropdown
-						var image_metadata_counts = read_metadata_analysis(path + "/" + "image_output_json_analysis.txt")
-						populate_dropdown_with_filters(image_metadata_counts, $metadata_filters)
-						
-					elif file_name.get_extension() == "ogv":
-						
-						# metadata filters dropdown
-						var video_metadata_counts = read_metadata_analysis(path + "/" + "video_output_json_analysis.txt")
-						populate_dropdown_with_filters(video_metadata_counts, $metadata_filters)
-						
-						# Video file handling
-						#print_debug("Video is here")
-						var hidden_screen = TextureRect.new()
-						cont.add_child(hidden_screen)
-						
-						var video_player = VideoStreamPlayer.new()
-						hidden_screen.add_child(video_player)
-						hidden_screen.hide()
-						video_player.stream = load(path + "/" + file_name)
-						video_player.play()
-						
-						# commented as unfinished thumbnail loading causes crash when up button pressed for some reason
-						#await get_tree().create_timer(0.001).timeout  # Wait for the first frame to be rendered
-						
-						print_debug(str(video_player))
-						var thumbnail = video_player.get_video_texture().get_image()
-						var thumbnail_texture = ImageTexture.create_from_image(thumbnail)
-						thumbnail_texture.set_size_override(Vector2i(128, 128))
-						texture_rect.texture = thumbnail_texture
-						
-						video_player.queue_free()
-						
-				var nBut = Button.new()
-				nBut.text = file_name
-				file_container.add_child(nBut)
-				nBut.pressed.connect(open_file.bind(file_name))
-
-				if limited.size() > 0:
-					if !file_name.get_extension() in limited:
-						file_container.queue_free()
+				var file_extension = file_name.get_extension()
+				if file_extension in limited:
+					var file_json = path + "/" + file_name.get_basename() + "_prediction.json"
+					print_debug(file_json)
+					#FileAccess.file_exists(file_json):
+					var predictions = read_metadata_file(file_json)
+					if selected_filter == "no filter" or selected_filter in predictions.keys():
+						print_debug("displaying files")
+						display_file(file_name, file_extension, cont)
+					else:
+						print_debug(predictions.keys())
 
 		file_name = dir.get_next()
 
+	# Update dropdown with filters
+	var metadata_counts = {}
+	if repopulate_dropdown == true:
+		for file in dir.get_files():
+			if file.ends_with("_analysis.txt"):
+				var file_metadata_counts = read_metadata_analysis(path + "/" + file)
+				for label in file_metadata_counts.keys():
+					if label in metadata_counts:
+						metadata_counts[label] += file_metadata_counts[label]
+					else:
+						metadata_counts[label] = file_metadata_counts[label]
+		
+	if repopulate_dropdown == true: 
+		repopulate_dropdown = false
+		populate_dropdown_with_filters(metadata_counts, $metadata_filters)
+
+
+func display_file(file_name, file_extension, parent_container):
+	var file_container = VBoxContainer.new()
+	parent_container.add_child(file_container)
+
+	var texture_rect = TextureRect.new()
+	file_container.add_child(texture_rect)
+
+	if file_extension == "jpg" or file_extension == "png":
+		var image_texture = ImageTexture.create_from_image(Image.load_from_file(path + "/" + file_name))
+		image_texture.set_size_override(Vector2i(128, 128))
+		texture_rect.texture = image_texture
+
+	elif file_extension == "ogv":
+		var hidden_screen = TextureRect.new()
+		parent_container.add_child(hidden_screen)
+
+		var video_player = VideoStreamPlayer.new()
+		hidden_screen.add_child(video_player)
+		hidden_screen.hide()
+		video_player.stream = load(path + "/" + file_name)
+		video_player.play()
+
+		await get_tree().create_timer(0.001).timeout  # Wait for the first frame to be rendered
+
+		var thumbnail = video_player.get_video_texture().get_image()
+		var thumbnail_texture = ImageTexture.create_from_image(thumbnail)
+		thumbnail_texture.set_size_override(Vector2i(128, 128))
+		texture_rect.texture = thumbnail_texture
+
+		video_player.queue_free()
+
+	var nBut = Button.new()
+	nBut.text = file_name
+	file_container.add_child(nBut)
+	nBut.pressed.connect(open_file.bind(file_name))
+
+
+func read_metadata_file(file_path: String) -> Dictionary:
+	var predictions = {}
+	
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null:
+		push_error("Failed to open .json file: " + file_path)
+		return predictions
+	
+	var json_string = file.get_as_text()
+	var data_json = JSON.parse_string(json_string)
+	file.close()
+	
+	if "predictions" in data_json:
+		for prediction in data_json["predictions"]:
+			predictions[prediction["label"]] = prediction["probability"]
+	else:
+		push_error("No 'predictions' key found in JSON file: " + file_path)
+	#print_debug("Predictions is: " + str(predictions))
+	
+	return predictions
+	
 
 func read_metadata_analysis(file_path: String) -> Dictionary:
 	var metadata_counts = {}
@@ -150,6 +187,7 @@ func read_metadata_analysis(file_path: String) -> Dictionary:
 
 
 func populate_dropdown_with_filters(metadata_counts: Dictionary, dropdown: OptionButton):
+	print_debug("Populating dropdown")
 	# Clear existing options
 	dropdown.clear()
 	
@@ -159,13 +197,13 @@ func populate_dropdown_with_filters(metadata_counts: Dictionary, dropdown: Optio
 
 	# Add filter options to the dropdown menu
 	for label in metadata_counts.keys():
-		dropdown.add_item(label + " [" + str(metadata_counts[label]) + "]")
+		dropdown.add_item(label)
 
 func _on_up_pressed(): # for some reason it crashes when videos dir/thumbnails not finished loading all
 	if file: path = path.get_base_dir()
 	path = path.get_base_dir()
 	$metadata_filters.clear()
-	set_layout()
+	set_layout("", true)
 
 func _on_path_text_submitted(new_text:String):
 	if new_text.is_absolute_path():
@@ -209,3 +247,13 @@ func _on_new_folder_pressed():
 
 func _on_search_bar_text_submitted(new_text): # used submitted instead of changed due to perfomances issue from reloading thumbnails (typing too slow)
 	set_layout(new_text)
+
+
+func _on_metadata_filters_item_selected(index):
+	selected_filter_index = index
+	set_layout("", false)
+
+
+func _on_filter_check_toggled(button_pressed):
+	print_debug("filter check button is:" + str(button_pressed))
+	set_layout("", true)
